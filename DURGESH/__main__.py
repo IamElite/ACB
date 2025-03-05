d806c0ec8ddade187fd8e683cc6c085728f2df5b
Dvis <gokhaledurgesh4@gmail.com>
2025-03-05 18:07:48 +0530

    Update __main__.py

--- DURGESH/__main__.py
+++ DURGESH/__main__.py
@@ -2,15 +2,21 @@ import importlib
 
 from pyrogram import idle
 
+import config
 from DURGESH import app
 from DURGESH.modules import ALL_MODULES
 
 async def boot():
     await app.start()
-
+    
+    try:
+        owner_id = int(config.OWNER_ID)
+        await app.send_message(owner_id, f"{app.mention} has started")
+    except Exception as ex:
+        print("Error sending startup message to owner:", ex)
+    
     for module in ALL_MODULES:
         importlib.import_module(f"DURGESH.modules.{module}")
-
     await idle()
     await app.stop()
 
