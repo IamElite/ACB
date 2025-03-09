4aae1410e93fcdc3eea5ce63820acc5c042d98bf
Dvis <gokhaledurgesh4@gmail.com>
2025-03-09 13:05:37 +0530

    Update pypi.py

--- DURGESH/modules/pypi.py
+++ DURGESH/modules/pypi.py
@@ -1,8 +1,9 @@
 import requests
-from pyrogram import Client, filters
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from DURGESH import app 
+from DURGESH import app
+
 
 def get_pypi_info(package_name: str):
     try:
@@ -17,11 +18,9 @@ def get_pypi_info(package_name: str):
 
 @app.on_message(filters.command("pypi", prefixes="/"))
 async def pypi_info_command(client, message):
-    # Command mein package name check karo
     if len(message.command) < 2:
         await message.reply_text("Please provide a package name after the /pypi command.")
         return
-
     package_name = message.command[1]
     pypi_info = get_pypi_info(package_name)
 
@@ -48,6 +47,10 @@ async def pypi_info_command(client, message):
 
 @app.on_callback_query(filters.regex("^close$"))
 async def close_callback(client, callback_query):
-    if callback_query.message:
-        await callback_query.message.delete()
-    await callback_query.answer()
+    try:
+        if callback_query.message:
+            # Instead of deleting, edit the message to indicate it's closed.
+            await callback_query.message.edit_text("Closed")
+        await callback_query.answer("Closed")
+    except Exception as e:
+        print(f"Error in close_callback: {e}")
