9d1a5967e216107ff0c871d0edef00f9d41c93e4
Dvis <gokhaledurgesh4@gmail.com>
2025-03-09 13:26:18 +0530

    Update pypi.py

--- DURGESH/modules/pypi.py
+++ DURGESH/modules/pypi.py
@@ -12,14 +12,14 @@ def get_pypi_info(package_name: str):
         if response.status_code == 200:
             return response.json()
     except Exception as e:
-        print(f"Error fetching PyPI information: {e}")
+        print(f"Eʀʀᴏʀ ғᴇᴛᴄʜɪɴɢ PʏPI ɪɴғᴏʀᴍᴀᴛɪᴏɴ: {e}")
     return None
 
 
 @app.on_message(filters.command("pypi", prefixes="/"))
 async def pypi_info_command(client, message):
     if len(message.command) < 2:
-        await message.reply_text("Please provide a package name after the /pypi command.")
+        await message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴘᴀᴋᴀɢᴇ ɴᴀᴍᴇ ᴀꜰᴛᴇʀ ᴛʜᴇ /pypi ᴄᴏᴍᴍᴀɴᴅ.")
         return
     package_name = message.command[1]
     pypi_info = get_pypi_info(package_name)
@@ -27,30 +27,29 @@ async def pypi_info_command(client, message):
     if pypi_info:
         info = pypi_info.get("info", {})
         project_urls = info.get("project_urls", {})
-        homepage = project_urls.get("Homepage", "N/A")
+        homepage = project_urls.get("Homepage", "ɴ/ᴀ")
 
         info_message = (
             f"ᴅᴇᴀʀ {message.from_user.mention}\n"
-            f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴘᴀᴋᴀɢᴇ ᴅᴇᴛᴀɪʟs:\n\n"
-            f"ᴘᴀᴋᴀɢᴇ ɴᴀᴍᴇ ➪ {info.get('name', 'N/A')}\n"
-            f"ʟᴀᴛᴇsᴛ ᴠᴇʀsɪᴏɴ ➪ {info.get('version', 'N/A')}\n"
-            f"ᴅᴇsᴄʀɪᴘᴛɪᴏɴ ➪ {info.get('summary', 'N/A')}\n"
+            f"ʜᴇʀᴇ ɪꜱ ʏᴏᴜʀ ᴘᴀᴋᴀɢᴇ ᴅᴇᴛᴀɪʟꜱ:\n\n"
+            f"ᴘᴀᴋᴀɢᴇ ɴᴀᴍᴇ ➪ {info.get('name', 'ɴ/ᴀ')}\n"
+            f"ʟᴀᴛᴇꜱᴛ ᴠᴇʀꜱɪᴏɴ ➪ {info.get('version', 'ɴ/ᴀ')}\n"
+            f"ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ ➪ {info.get('summary', 'ɴ/ᴀ')}\n"
             f"ᴘʀᴏᴊᴇᴄᴛ ᴜʀʟ ➪ {homepage}"
         )
         markup = InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="〆 ᴄʟᴏsᴇ 〆", callback_data="close")]]
+            [[InlineKeyboardButton(text="〆 ᴄʟᴏꜱᴇ 〆", callback_data="close")]]
         )
         await message.reply_text(info_message, reply_markup=markup)
     else:
-        await message.reply_text(f"Package '{package_name}' not found. Please try again later.")
+        await message.reply_text(f"ᴘᴀᴋᴀɢᴇ '{package_name}' ɴᴏᴛ ꜰᴏᴜɴᴅ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
 
 
 @app.on_callback_query(filters.regex("^close$"))
 async def close_callback(client, callback_query):
     try:
-        if callback_query.message:
-            # Instead of deleting, edit the message to indicate it's closed.
-            await callback_query.message.edit_text("Closed")
-        await callback_query.answer("Closed")
+        if callback_query.message is not None:
+            await callback_query.message.delete()
     except Exception as e:
-        print(f"Error in close_callback: {e}")
+        print(f"Eʀʀᴏʀ ᴅᴇʟᴇᴛɪɴɢ ᴍᴇssᴀɢᴇ: {e}")
+    await callback_query.answer()
