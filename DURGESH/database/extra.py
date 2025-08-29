c5bb8c6d9cdeac8ef1859381e561ca2495d2fe12
IamElite <elitemindbusiness@gmail.com>
2025-08-29 18:59:05 +0530

    .

--- DURGESH/database/extra.py
+++
@@ -1,9 +0,0 @@
-from DURGESH import app
-
-protected_messages = {}
-
-
-async def protect_message(chat_id, message_id):
-    if chat_id not in protected_messages:
-        protected_messages[chat_id] = []
-    protected_messages[chat_id].append(message_id)
