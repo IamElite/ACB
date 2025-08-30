6e6c88a7342a7c960371067350e605bd4d324fcb
IamElite <elitemindbusiness@gmail.com>
2025-08-30 09:31:22 +0530

    Create eco.py

---
+++ DURGESH/modules/eco.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from DURGESH import app
+
+
+@app.on_message(
+    filters.command(["eco", "e"], prefixes=["/", "!", ".", ""])
+    & filters.reply
+    & filters.user(ADMINS)
+)
+async def eco_reply(_, m):
+    if not (r := m.reply_to_message):
+        return await m.reply("Reply to a message.")
+    if len(parts := m.text.split(maxsplit=1)) < 2:
+        return await m.reply("Give text to echo.")
+    await m.delete()
+    await r.reply(parts[1])
