afc9b290597eb996a1301be5f5247cac7ac40d43
IamElite <elitemindbusiness@gmail.com>
2025-09-09 17:45:09 +0530

    Update __init__.py

--- DURGESH/__init__.py
+++ DURGESH/__init__.py
@@ -36,6 +36,7 @@ class Bot(Client):
             api_hash=API_HASH,
             workers=TG_BOT_WORKERS,
             bot_token=BOT_TOKEN,
+            upload_timeout=600,
         )
 
     async def start(self, *args, **kwargs):
@@ -61,3 +62,4 @@ class Bot(Client):
         return f"[{self.name}](tg://user?id={self.id})"
 
 app = Bot()
+
