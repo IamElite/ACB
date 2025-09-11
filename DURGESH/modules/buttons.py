6798794d8e988c3a48707f0d066f9b76f91902ed
IamElite <elitemindbusiness@gmail.com>
2025-09-11 22:19:25 +0530

    Update buttons.py

--- DURGESH/modules/buttons.py
+++ DURGESH/modules/buttons.py
@@ -69,14 +69,23 @@ async def unauth_channel_cmd(client, message: Message):
 
 def parse_buttons(text: str):
     keyboard = []
-    for line in text.strip().splitlines():
-        btns = []
-        matches = re.findall(r"\[([^+\]]+?)\s*\+\s*(https?://[^\]\s]+)\]", line)
-        for label, link in matches:
-            btns.append(InlineKeyboardButton(label.strip(), url=link.strip()))
-        if btns:
-            keyboard.append(btns)
+    matches = re.findall(r"\[([^+\]]+?)\s*\+\s*(https?://[^\]\s]+)\]", text)
+
+    row = []
+    for i, (label, link) in enumerate(matches, start=1):
+        row.append(InlineKeyboardButton(label.strip(), url=link.strip()))
+
+        # Agar row me 8 buttons ho gaye to nayi row bana do
+        if i % 8 == 0:
+            keyboard.append(row)
+            row = []
+
+    # Last row agar khali nahi hai to bhi add kar do
+    if row:
+        keyboard.append(row)
+
     return InlineKeyboardMarkup(keyboard) if keyboard else None
+
 
 # -------------------- CHANGE BUTTON (NEW METHOD) -------------------- #
 
